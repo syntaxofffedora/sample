b7c787db13e7f06838d8633eb8f62e3630864edd
fedora syntaxoff <syntaxofffedora@yandex.ru>
2024-11-29 06:00:04 +0300

    mod index.html, js/main.js

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@ $(function(){
         $('.popup-container').css('display', 'none');
     };
 
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
-    };
 
     $('.close').on('click', function(){
         clearForm();
